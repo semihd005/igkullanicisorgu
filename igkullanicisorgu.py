8850fd4c3855dc6d45bd787ab90c5685916fe059
BYdoktor <82740545+semihd005@users.noreply.github.com>
2022-05-14 22:54:41 +0300

    Update igkullanicisorgu.py

--- igkullanicisorgu.py
+++ igkullanicisorgu.py
@@ -1,26 +1,22 @@
-from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
-
 URL = "https://www.instagram.com/"
 
-def verileri_al(kullanici_adi):
-    son_url = URL + kullanici_adi
+def verileri_al(kullanici_adi)
+son_url = URL + kullanici_adi
 
-    request = Request(son_url, headers={'User-Agent':'Mozilla/5.0'})
-    html_verisi = urlopen(request).read()
+request = Request(son_url, headers={'User-Agent':'Mozilla/5.0'})
+html_verisi = urlopen(request).read()
 
-    soup = BeautifulSoup(html_verisi, 'html.parser')
+soup = BeautifulSoup(html_verisi, 'html.parser')
 
-    veri = soup.find("meta",property="og:description").attrs['content']
+veri = soup.find("meta",property="og:description").attrs['contet']
 
-    veri = veri.split("-")[0]
+veri = veri.split("-") [0]
 
-    veri = veri.split(" ")
+veri = veri.split(" ")
 
-    print("takipci sayisi: "+veri[0])
-    print("takip edilen sayisi: "+veri[2])
-    print("gonderi sayisi: "+veri[4])
-
+print("takipci sayisi: "+veri[0])
+print("takip edilen sayisi: "+veri[2])
+print("gonderi sayisi: "+veri[4])
 
 kullanici_adi = input("kullanici adi giriniz: ")
 verileri_al(kullanici_adi)
